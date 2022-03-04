6fdfe86bed71a43e9ca4fb8beec7bbfab736014a
Hennessy <htorres2@stevens.edu>
2022-03-04 10:37:55 -0500

    hw 5

--- Triangle.py
+++ Triangle.py
@@ -8,42 +8,45 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: js
 """
 
-def classifyTriangle(a, b, c):
+def classify_triangle(a, b, c):
     """
     This function returns a string with the type of triangle from three integer values
     corresponding to the lengths of the three sides of the Triangle.
-    
+
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
         If the sum of any two sides equals the square of the third side, then return 'Right'
-      
+
       BEWARE: there may be a bug or two in this code
-    """ 
-    if a <= 0 or b <= 0 or c <= 0:
-        return 'InvalidInput'
-    
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    """
+# verify that all 3 inputs are integer
+# Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int)) or not(isinstance(b,int)) or not(isinstance(c,int)):
         return 'InvalidInput'
-    
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return 'InvalidInput'
+
+    if a > 200 or b > 200 or c > 200:
+        return 'InvalidInput'
+
     # checks for false/true as they extend int in python but won't work here
     if a is False or b is False or c is False:
       return 'InvalidInput'
     if a is True or b is True or c is True:
       return 'InvalidInput'
-      
-    # This information was not in the requirements spec but 
+
+    # This information was not in the requirements spec
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
     if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
+
+    # now we know that we have a valid triangle
     if a == b and b == a and c == b:
         return 'Equilateral'
     # have to do this for all three sides as we don't know which is the hypotenuse
@@ -55,4 +58,6 @@ def classifyTriangle(a, b, c):
         return 'Right'
     if (a != b) and  (b != c) and (a != c):
         return 'Scalene'
+
+
     return 'Isoceles'
